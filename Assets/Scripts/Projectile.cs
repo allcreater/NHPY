ef209c42e8f9aae5a9aae6ad400237ca1af178ac
Anton Semenov <allcreater@gmail.com>
2018-11-19 04:06:33 +0300

    very basic shooting

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+public class Projectile : MonoBehaviour 
+{
+    public string[] tags;
+
+    // Use this for initialization
+    void Start () 
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(gameObject.tag);
+        if (!tags.Contains(gameObject.tag))
+            return;
+
+        GameObject.Destroy(gameObject);
+
+        //GetComponent<Rigidbody>().velocity += Vector3.up * 10;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour 
+{
+    public GameObject bulletPrototype;
+    public float bulletSpeed = 10.0f;
+    public float reloadTime = 0.5f;
+
+    private float reloadingTimer = 0.0f;
+    // Use this for initialization
+    void Start () 
+    {
+        
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetAxis("Fire1") > 0.0f && reloadingTimer > reloadTime)
+        {
+            var bullet = GameObject.Instantiate(bulletPrototype, transform.position, Random.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+
+            reloadingTimer = 0.0f;
+        }
+
+        reloadingTimer += Time.deltaTime;
+    }
+}
