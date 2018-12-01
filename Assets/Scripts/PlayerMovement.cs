4c5c47c934ceecaf14b83659ac275415cb4068f7
BlazerDom <blazerdom@yandex.ru>
2018-12-01 03:40:35 +0300

    Revert "Merge branch 'dev' of https://github.com/allcreater/NHPY into dev"
    This reverts commit c6dbbd9cf87eadcf453ce5b9c89cfc15997c2d84, reversing
    changes made to fd72c6f95b62837315ab57803cb92f1caa314765.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,61 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnitySampleAssets.CrossPlatformInput;
 
-public static class MathExtension
-{
-}
-
 public class PlayerMovement : MonoBehaviour
 {
-    public float walkSpeed;
-    public float runSpeed;
-    public float staminaConsumption;
+    public float movementSpeed;
 
     private Animator animator;          // Reference to the animator component.
     private new Rigidbody rigidbody;    // Reference to the player's rigidbody.
-    private PlayerStats stats;
 
     private Vector3 m_moveDirection;
-    private float m_actualSpeed;
-    private float m_speedBoost;
 
     private void Awake()
     {
         animator = GetComponentInChildren<Animator>();
         rigidbody = GetComponent<Rigidbody>();
-        stats = GetComponent<PlayerStats>();
-
-        m_speedBoost = runSpeed / walkSpeed;
-        if (m_speedBoost < 1)
-            throw new System.Exception("runSpeed must be bigger than walk speed!");
     }
 
-    void MoveTo(MoveToParams moveParams)
+    void MoveTo(Vector3 direction)
     {
-        m_moveDirection = Vector3.ClampMagnitude(moveParams.direction, 1);
-        if (moveParams.isRun)
-            m_moveDirection *= m_speedBoost;
+        m_moveDirection = Vector3.ClampMagnitude(direction, 1.0f);
     }
 
     private void Update()
     {
+        animator.SetFloat("Speed", m_moveDirection.magnitude);
     }
 
     private void FixedUpdate ()
     {
-        var length = m_moveDirection.magnitude;
-        var direction = m_moveDirection / (length + 0.001f);
-
-        if (length > 1.01)
-        {
-            float staminaOverrun = (length - 1) / (m_speedBoost - 1) * staminaConsumption;
-            float amplification = stats.consumeStamina(staminaOverrun * Time.deltaTime);
-
-            length = Mathf.Clamp(length, 0, 1 + m_speedBoost * amplification);
-        }
-
-        animator.SetFloat("Speed", length);
-        rigidbody.MovePosition(rigidbody.position + direction * ( length * walkSpeed *  Time.fixedDeltaTime));
+        rigidbody.MovePosition(rigidbody.position + m_moveDirection * movementSpeed * Time.fixedDeltaTime);
 
         if (m_moveDirection.sqrMagnitude > 0.0)
         {
