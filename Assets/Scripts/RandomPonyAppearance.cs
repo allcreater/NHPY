e07f56f213ce9daf0b45f4f3ea4b1200e991821c
Anton Semenov <allcreater@gmail.com>
2018-12-20 02:05:12 +0300

    made mane and tail colors more configurable

--- Assets/Scripts/RandomPonyAppearance.cs
+++ Assets/Scripts/RandomPonyAppearance.cs
@@ -16,19 +16,25 @@ public class RandomPonyAppearance : MonoBehaviour
     public float saturationMin = 0.5f, saturationMax = 1.0f;
     public float valueMin = 0.5f, valueMax = 1.0f;
 
+    [Header("random mane and tail color")]
+    public float maneSaturationMin = 0.5f, maneSaturationMax = 1.0f;
+    public float maneValueMin = 0.5f, maneValueMax = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        var maneTailColor = Random.ColorHSV(0.0f, 1.0f, maneSaturationMin, maneSaturationMax, maneValueMin, maneValueMax);
+
         if (manes != null && manes.Length > 0)
         {
             var maneInstance = GameObject.Instantiate(manes[Random.Range(0, manes.Length)], maneSocket ?? transform);
-            maneInstance.GetComponent<Renderer>().materials[0].color = Random.ColorHSV(0.0f, 1.0f, saturationMin, saturationMax, valueMin, valueMax);
+            maneInstance.GetComponent<Renderer>().materials[0].color = maneTailColor;
         }
 
         if (tails != null && tails.Length > 0)
         {
             var tailInstance = GameObject.Instantiate(tails[Random.Range(0, tails.Length)], tailSocket ?? transform);
-            tailInstance.GetComponent<Renderer>().materials[0].color = Random.ColorHSV(0.0f, 1.0f, saturationMin, saturationMax, valueMin, valueMax);
+            tailInstance.GetComponent<Renderer>().materials[0].color = maneTailColor;
         }
 
         if (bodyObject)
