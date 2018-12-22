fce6d83f8686556ec1ce6410d1e8e3a39c89957e
Anton Semenov <allcreater@gmail.com>
2018-12-22 03:56:04 +0300

    Changes gift explosion and simple change in scene

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -8,7 +8,8 @@ public class Projectile : MonoBehaviour
 {
     public string[] tags;
     public GameObject hitEffect;
-    public float delay = 5;
+    public float startDelay = 0.0f;
+    public float explodeDelay = 0.0f;
 
     // Use this for initialization
     void Start () 
@@ -23,18 +24,24 @@ public class Projectile : MonoBehaviour
         
         if (hitEffect)
         {
-            StartCoroutine(ExecuteAfterTime(delay, other, hitEffect));
+            StartCoroutine(ExecuteAfterTime(startDelay, explodeDelay, other, hitEffect));
         }
     }
 
-    IEnumerator ExecuteAfterTime(float time, Collider other, GameObject hitEffect)
+    IEnumerator ExecuteAfterTime(float startDelay, float explodeDelay, Collider other, GameObject hitEffect)
     {
-        yield return new WaitForSeconds(time);
+        if (!Mathf.Approximately(startDelay, 0.0f))
+            yield return new WaitForSeconds(startDelay);
+        GetComponent<Rigidbody>().velocity = Vector3.up * 20.0f;
+
+        if (!Mathf.Approximately(explodeDelay, 0.0f))
+            yield return new WaitForSeconds(explodeDelay);
+
+        Debug.Log("Hello!");
 
         var effectObject = GameObject.Instantiate(hitEffect, transform.position, transform.rotation, null);
         effectObject.SendMessage("CollidedWith", other);
             GameObject.Destroy(gameObject);
- 
     }
 
     // Update is called once per frame
