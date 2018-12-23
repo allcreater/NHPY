457d85c4b4c6115102445d73b8f7ea84d7fe729c
Anton Semenov <allcreater@gmail.com>
2018-12-23 23:08:14 +0300

    introduce preference support

---
+++ Assets/Scripts/Game/EnableLightByGameSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableLightByGameSettings : MonoBehaviour
+{
+    public Preferences.DynamicLightsAmount enableIfBetter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var light = GetComponent<Light>();
+        if (light)
+            light.enabled = Preferences.GameSettings.instance.dynamicLightsAmount >= enableIfBetter;
+    }
+
+}
