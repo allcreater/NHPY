16b71aca85e372efaedda2545881bf2568ea2db2
Anton Semenov <allcreater@gmail.com>
2018-12-24 00:38:42 +0300

    working around scene + hints at UI

---
+++ Assets/Scripts/UI/Message.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Message : MonoBehaviour
+{
+    public GameObject dialogPanel;
+    public Text messageTextBox;
+    public Image messageImage;
+
+    public void SetMessage(string message)
+    {
+        messageTextBox.text = message.Replace("<br>", "\n");
+        dialogPanel.SetActive(true);
+    }
+
+    public void SetImage(string imageName)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Auto pause
+        Time.timeScale = dialogPanel.activeInHierarchy ? 0.0f : 1.0f;
+    }
+}
---
+++ Assets/Scripts/UI/TriggerEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEvent onTrigger;
+    public string activatorTag = "Player";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != activatorTag)
+            return;
+
+        if (onTrigger != null)
+            onTrigger.Invoke();
+    }
+}
